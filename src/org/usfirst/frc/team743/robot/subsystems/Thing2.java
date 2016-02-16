fb18ee24acd0adfa6115ae0ce21d0801ee362cf2
Allan Herman <Teacher.Physics@gmail.com>
2016-02-16 13:34:30 -0500

    Programmed every Actuator except the Craw. In this Program though, every actuator has its own Talon. The bottom and top actuators are named Thing 1 and Thing 2 respectively :D

---
+++ src/org/usfirst/frc/team743/robot/subsystems/Thing2.java
@@ -0,0 +1,18 @@
+package org.usfirst.frc.team743.robot.subsystems;
+
+import edu.wpi.first.wpilibj.command.Subsystem;
+
+public class Thing1 extends Subsystem {
+
+	@Override
+	protected void initDefaultCommand() {
+		// TODO Auto-generated method stub
+
+	}
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
